386d9b65144976b9d5f9629e96540af8b51dfac5
Jeffrey Killeen <jtk12b@acu.edu>
2013-07-27 23:16:00 -0500

    Added more features to chatServer

--- ChatServer/chatserver.js
+++ ChatServer/chatserver.js
@@ -3,10 +3,12 @@ var net = require('net')
 var chatServer = net.createServer()
 
 chatServer.on('connection', function(client) {
-	client.write('Hi!\n');
-	client.write('Bye!\n');
-	
-	client.end()
+  client.write('Hi!\n');
+
+  client.on('data', function(data) {
+    console.log(data)
+  })
+
 })
 
 chatServer.listen(9000)
